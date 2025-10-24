cdd245771bdf5ac8944988cae544c7fcef8e2ec0
Roberto <sheypol132@gmail.com>
2025-10-24 12:31:14 -0600

    notific

--- backend/src/config/mail.php
+++ backend/src/config/mail.php
@@ -4,11 +4,11 @@
 // --- RELLENA ESTOS 4 DATOS DE TU CPANEL ---
 define('HOST_SMTP', 'mail.detpon.com');       // El Servidor Saliente que encontraste
 define('PUERTO_SMTP', 465);                   // El Puerto (465 o 587)
-define('USUARIO_SMTP', 'roberto@detpon.com'); // Tu correo
-define('PASSWORD_SMTP', 'RY.detpon612'); 
+define('USUARIO_SMTP', 'mcf@detpon.com'); // Tu correo
+define('PASSWORD_SMTP', 'sismonitoreo@2025'); 
 
 // --- DATOS DEL REMITENTE ---
-define('REMITENTE_EMAIL', 'roberto@detpon.com');
+define('REMITENTE_EMAIL', 'mcf@detpon.com');
 define('REMITENTE_NOMBRE', 'Sistema de Alertas');
 
 // --- ¡IMPORTANTE! Lista de correos que recibirán la alerta ---
